0cd8ce0d88e2b3cc143207e511ef0b8d923349bf
Mihailob5017 <mihailob5017@gmail.com>
2020-12-24 15:47:09 +0100

    setting up caching

--- sw.js
+++ sw.js
@@ -1,10 +1,29 @@
+const staticCacheName = 'site-static';
+const assets = [
+    '/',                // URL route
+    '/index.html',      // Files needed
+    '/js/app.js',
+    '/js/ui.js',
+    '/css/materialize.min.css',
+    '/css/styles.css',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    '/img/dish.png'
+]
 
 self.addEventListener('install', e => {
-    console.log('service worker installed');
-  
+
+    e.waitUntill(
+        caches.open(staticCacheName).then(cache => {
+            console.log('caching shell assets')
+            cache.addAll(assets)
+        }))
 
 })
 
 self.addEventListener('activate', e => {
     console.log('service worker has been activated')
+})
+
+self.addEventListener('fetch', e => {
+
 })
