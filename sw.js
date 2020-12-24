d2dfa072095e34dd46ec4ae58cda7fb400ef90b8
Mihailob5017 <mihailob5017@gmail.com>
2020-12-24 19:25:15 +0100

    dynamic caching for different pages

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 const staticCacheName = 'site-static-v2';
+const dynamicCache = 'site-dynamic-v1'
 const assets = [
     '/',                // URL route
     '/index.html',      // Files needed
@@ -24,11 +25,11 @@ self.addEventListener('activate', evt => {
 
     evt.waitUntil(
         caches.keys().then(keys => {
-           
+
             return Promise.all(
                 keys
                     .filter(key => key !== staticCacheName)
-                    .map( key => caches.delete(key))
+                    .map(key => caches.delete(key))
             )
         })
     )
@@ -40,7 +41,13 @@ self.addEventListener('fetch', evt => {
 
     evt.respondWith(
         caches.match(evt.request).then(cacheRes => {
-            return cacheRes || fetch(evt.request)
+            return cacheRes || fetch(evt.request).then(fetchRes => {
+                return caches.open(dynamicCache).then(cache => {
+                    // const resCopy=fetchRes
+                    cache.put(evt.request.url, fetchRes.clone())
+                    return fetchRes
+                })
+            })
         })
     )
 })
